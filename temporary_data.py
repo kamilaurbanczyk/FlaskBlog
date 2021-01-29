8a2c1f57cf58f6c5f2c2d9a1865e6435a6be5799
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2021-01-29 19:10:39 +0100

    Create file with data to use in articles 'index.html'

---
+++ temporary_data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': "Democracy (Greek: δημοκρατία, dēmokratiā, from dēmos 'people' and kratos 'rule') is a form of government in which the people have the authority to choose their governing legislators. The decisions on who is considered part of the people and how authority is shared among or delegated by the people have changed over time and at different speeds in different countries, but they have included more and more of the inhabitants of all countries. Cornerstones include freedom of assembly and speech, inclusiveness and equality, membership, consent, voting, right to life and minority rights.",
+            'author': 'Ben',
+            'date': '23-11-2020'
+         },
+        {
+            'id': 1,
+            'title': 'Article Two',
+            'body': "Democracy (Greek: δημοκρατία, dēmokratiā, from dēmos 'people' and kratos rule') is a form of government in which the people have the authority to choose their governing legislators. The decisions on who is considered part of the people and how authority is shared among or delegated by the people have changed over time and at different speeds in different countries, but they have included more and more of the inhabitants of all countries. Cornerstones include freedom of assembly and speech, inclusiveness and equality, membership, consent, voting, right to life and minority rights.",
+            'author': 'Joanne',
+            'date': '01-01-2021'
+        },
+        {
+            'id': 1,
+            'title': 'Article Three',
+            'body': "Democracy (Greek: δημοκρατία, dēmokratiā, from dēmos 'people' and kratos 'rule') is a form of government in which the people have the authority to choose their governing legislators. The decisions on who is considered part of the people and how authority is shared among or delegated by the people have changed over time and at different speeds in different countries, but they have included more and more of the inhabitants of all countries. Cornerstones include freedom of assembly and speech, inclusiveness and equality, membership, consent, voting, right to life and minority rights.",
+            'author': 'Tom',
+            'date': '28-12-2020'
+        }
+    ]
+    return articles
