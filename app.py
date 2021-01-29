c8a8181fb9e7b1f82bb727f11a1de260b1532a80
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2021-01-29 19:18:14 +0100

    use data from temporary_data.py in index.html to display articles

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from temporary_data import Articles
 
 app = Flask(__name__)
 
+Articles = Articles()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', articles=Articles)
 
 
 @app.route('/about')
