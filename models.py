665dfd783300e06e9ad7d1330c00ec5dc641100c
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2021-02-01 16:21:02 +0100

    edit models

--- models.py
+++ models.py
@@ -1,23 +1,22 @@
-from database import Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
-from sqlalchemy.orm import relationship
+from app import db
+from sqlalchemy import Integer, String, DateTime, ForeignKey, Text
 
 
-class User(Base):
+class User(db.Model):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=False)
-    username = Column(String(30), unique=True)
-    email = Column(String(100))
-    register_date = Column(DateTime)
-    articles = relationship('Article', backref='user', lazy=True)
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(50), unique=False)
+    username = db.Column(String(30), unique=True)
+    email = db.Column(String(100))
+    register_date = db.Column(DateTime)
+    articles = db.relationship('Article', backref='user', lazy=True)
 
 
-class Article(Base):
+class Article(db.Model):
     __tablename__ = 'articles'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(100))
-    author = Column(String(30))
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    body = Column(Text)
-    date = Column(DateTime)
+    id = db.Column(Integer, primary_key=True)
+    title = db.Column(String(100))
+    author = db.Column(String(30))
+    user_id = db.Column(Integer, ForeignKey('users.id'), nullable=False)
+    body = db.Column(Text)
+    date = db.Column(DateTime)
