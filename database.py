fe130f61011642c6f57b390174328df83c546b3e
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2021-02-01 15:09:11 +0100

    edit database.py

--- database.py
+++ database.py
@@ -2,3 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine('mysql://malami:78z433XMn@localhost/myflaskblog')
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
