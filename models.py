9799159083af19fe2d45d29305f01ed1798d6211
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2021-02-01 15:48:49 +0100

    Create class Article in models.py, create relationskip between user and article models

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
@@ -9,4 +10,14 @@ class User(Base):
     username = Column(String(30), unique=True)
     email = Column(String(100))
     register_date = Column(DateTime)
-    
+    articles = relationship('Article', backref='user', lazy=True)
+
+
+class Article(Base):
+    __tablename__ = 'articles'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    author = Column(String(30))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    body = Column(Text)
+    date = Column(DateTime)
